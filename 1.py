4860daea73bd09e6abfd371ddb6bf72cf026c66b
heroku1heroku1 <heroku1heroku1@yandex.ru>
2019-07-13 15:31:56 +0300

    Procfile

---
+++ 1.py
@@ -0,0 +1,35 @@
+import telebot
+from telebot import types
+
+API_TOKEN = '871426689:AAHjCbL5mmk_NyJWj9G_OhQxUjf1-T-jMzU'
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+
+
+markup_inline_url2 = types.InlineKeyboardMarkup()
+btn_in_url2 = types.InlineKeyboardButton('Права и обязанности сторон', url='https://note-pad.net/ru/read/fd3d573508e0c70d56cb3490ae25c147?page=1')
+markup_inline_url2.add(btn_in_url2)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+ print(message)
+ bot.send_message(message.chat.id, "*ДdfdfF4B1*                                                             "
+                                   "\U00002796\U00002796\U00002796\U00002796\U00002796\U00002796\U00002796\U00002796\U00002796\U00002796\U00002796\U00002796\U00002796                                                "
+                                   "Обменdsfgdsgsdfgel.                                                          "
+                                   "\U00002796\U00002796\U00002796\U00002796\U00002796\U00002796\U00002796\U00002796\U00002796\U00002796\U00002796\U00002796\U00002796                                                "
+                                   "Выберите подходящий пункт меню: ", parse_mode="Markdown")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+ print(message)
+ if message.text == "Оdfgd5":
+  bot.send_message(message.chat.id, "fgdgdfdg")
+
+
+
+bot.polling()
+
